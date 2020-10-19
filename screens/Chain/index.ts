c688a659c1fcf0763ffa2c0d11a08d4a12e511c4
Erik <kimseyerik@gmail.com>
2020-10-19 16:06:42 -0400

    Commented out nonexisting modules in screen/chain/index

--- screens/Chain/index.ts
+++ screens/Chain/index.ts
@@ -1,7 +1,7 @@
-export {default as BaselineAssessment} from './BaselineAssessment';
-export {default as PrepareMaterials} from './PrepareMaterials';
-export {default as Reward} from './Reward';
-export {default as StepScreen} from './Step';
-export {default as Tipstricks} from './TipsTricks';
+// export {default as BaselineAssessment} from './BaselineAssessment';
+// export {default as PrepareMaterials} from './PrepareMaterials';
+// export {default as Reward} from './Reward';
+// export {default as StepScreen} from './Step';
+// export {default as Tipstricks} from './TipsTricks';
 export {default as ScoreCardScreen} from './ScoreCardScreen';
 
