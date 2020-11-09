e65226cb566ca2c1d999bc8630cdbed47740c220
Aaron Louie <ajlouie@gmail.com>
2020-11-09 14:18:59 -0500

    Fixes test config

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,6 @@ module.exports = {
     "transformIgnorePatterns": [
         "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)"
     ],
-    "transform": {
-        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/ts-jest"
-    },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     collectCoverage: true,
     collectCoverageFrom: [
