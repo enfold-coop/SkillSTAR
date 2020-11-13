4e53fa89e16b2d7e52afc11585ee58d3dde215f0
Erik <kimseyerik@gmail.com>
2020-11-13 16:48:12 -0500

    Created Session interface

--- types/StepAttempt.ts
+++ types/StepAttempt.ts
@@ -8,4 +8,8 @@ interface Attempt {
 
 class StepAttempt implements Attempt {
 	date: Date = new Date();
+	wasPrompted: boolean = false;
+	promptLevel: number = 0;
+	behavOccurred: boolean = false;
+	behavSeverity: number = 0;
 }
