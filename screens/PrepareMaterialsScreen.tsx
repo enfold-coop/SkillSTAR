d9010d7d21e333c64c2373a5b19e9cdb3f0b2e84
Erik <kimseyerik@gmail.com>
2020-11-16 09:56:56 -0500

    Styles adjusted to ChainsHomeScreen, PrepMatScreen, AppHeader.  Hardcoded PrepMat content

--- screens/PrepareMaterialsScreen.tsx
+++ screens/PrepareMaterialsScreen.tsx
@@ -1,10 +1,11 @@
 import React, { FC, useState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Button, Card } from "react-native-paper";
+import { Button, Card, Title } from "react-native-paper";
 import { RootNavProps } from "../navigation/root_types";
 import CustomColors from "../styles/Colors";
 import { PREP_MATS } from "../data/prep_materials";
+import AppHeader from "../components/Header/AppHeader";
 
 type Props = {
 	route: RootNavProps<"PrepareMaterialsScreen">;
@@ -17,37 +18,57 @@ const PrepareMaterialsScreen: FC<Props> = (props) => {
 
 	return (
 		<View style={styles.container}>
-			<Text style={styles.headline}>
-				Have you gathered your materials?
-			</Text>
-			<View style={styles.listItem}>
-				<Image
-					style={styles.itemIcon}
-					source={require("../assets/images/prep_materials_icon/toothbrush.png")}
-				/>
-			</View>
-			<View style={styles.listItem}>
-				<Image
-					style={styles.itemIcon}
-					source={require("../assets/images/prep_materials_icon/toothpaste.png")}
-				/>
-			</View>
-			<View style={styles.listItem}>
-				<Image
-					style={styles.itemIcon}
-					source={require("../assets/images/prep_materials_icon/towel.png")}
-				/>
-			</View>
-			<View style={styles.listItem}>
-				<Image
-					style={styles.itemIcon}
-					source={require("../assets/images/prep_materials_icon/toothbrush.png")}
-				/>
-			</View>
+			<AppHeader name="Prepare Materials" />
+			<Card style={styles.listItem}>
+				<View style={styles.listItem}>
+					<Image
+						style={styles.itemIcon}
+						source={require("../assets/images/prep_materials_icon/toothbrush.png")}
+					/>
+					<Title style={styles.itemTitle}>Tooth Brush</Title>
+				</View>
+			</Card>
+			<Card style={styles.listItem}>
+				<View style={styles.listItem}>
+					<Image
+						style={styles.itemIcon}
+						source={require("../assets/images/prep_materials_icon/toothpaste.png")}
+					/>
+					<Title style={styles.itemTitle}>Tooth Paste</Title>
+				</View>
+			</Card>
+			<Card style={styles.listItem}>
+				<View style={styles.listItem}>
+					<Image
+						style={styles.itemIcon}
+						source={require("../assets/images/prep_materials_icon/towel.png")}
+					/>
+					<Title style={styles.itemTitle}>Towel</Title>
+				</View>
+			</Card>
+			<Card style={styles.listItem}>
+				<View style={styles.listItem}>
+					<Image
+						style={styles.itemIcon}
+						source={require("../assets/images/prep_materials_icon/water.png")}
+					/>
+					<Title style={styles.itemTitle}>Cup of Water</Title>
+				</View>
+			</Card>
+			<Card style={styles.listItem}>
+				<View style={styles.listItem}>
+					<Image
+						style={styles.itemIcon}
+						source={require("../assets/images/prep_materials_icon/medicine.png")}
+					/>
+					<Title style={styles.itemTitle}>Cabinet</Title>
+				</View>
+			</Card>
 			<Button
 				mode="contained"
 				color={CustomColors.uva.blue}
 				style={styles.nextBtn}
+				labelStyle={{ fontSize: 20 }}
 				onPress={() => {
 					navigation.navigate("StepScreen");
 				}}
@@ -61,33 +82,42 @@ const PrepareMaterialsScreen: FC<Props> = (props) => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		padding: 10,
+		padding: 20,
 		paddingTop: 30,
-		alignContent: "center",
-		justifyContent: "center",
+		alignContent: "flex-start",
+		justifyContent: "flex-start",
 	},
 	headline: {
 		fontSize: 20,
 	},
 	listItem: {
-		margin: 10,
-		padding: 10,
+		marginTop: 10,
+		marginLeft: 10,
 		flexDirection: "row",
 		justifyContent: "flex-start",
-		alignContent: "space-around",
+		alignContent: "stretch",
 	},
 	itemIcon: {
 		width: 80,
 		height: 80,
+		margin: 20,
+		marginLeft: 40,
+		marginRight: 130,
 	},
 	itemTitle: {
-		width: 200,
-		padding: 10,
+		// width: 200,
+		fontSize: 34,
+		lineHeight: 34,
+		alignSelf: "center",
+		fontWeight: "400",
 	},
 	nextBtn: {
-		margin: 20,
-		width: 122,
-		alignSelf: "center",
+		padding: 10,
+		fontSize: 24,
+		margin: 10,
+		marginRight: 0,
+		width: 222,
+		alignSelf: "flex-end",
 	},
 });
 
