fa630fe615f18450bb524b67647b36f65307ddc5
Erik <kimseyerik@gmail.com>
2020-11-19 13:38:49 -0500

    Adding input fields to bascreen

--- screens/BaselineAssessmentScreen.tsx
+++ screens/BaselineAssessmentScreen.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import { TextInput, Checkbox, Button, RadioButton } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 import { RootNavProps } from "../navigation/root_types";
 import { Session } from "../types/CHAIN/Session";
+import CustomColors from "../styles/Colors";
 
 type Props = {
 	route: RootNavProps<"BaselineAssessmentScreen">;
@@ -13,8 +14,22 @@ type Props = {
 
 const BaselineAssessmentScreen: FC<Props> = (props) => {
 	const navigation = useNavigation();
-
+	let [stepIndex, setStepIndex] = useState(0);
+	let [readyToSubmit, setReadyToSubmit] = useState(false);
+	let [text, setText] = useState("");
 	let { session } = props.route.params;
+
+	const incrIndex = () => {
+		stepIndex += 1;
+		setStepIndex(stepIndex);
+	};
+
+	const decIndex = () => {
+		if (stepIndex > 0) {
+			stepIndex -= 1;
+			setStepIndex(stepIndex);
+		}
+	};
 
 	/**
 	 * 1. get attempts array
@@ -31,16 +46,54 @@ const BaselineAssessmentScreen: FC<Props> = (props) => {
 			style={styles.image}
 		>
 			<View style={styles.container}>
-				<Text></Text>
+				<View style={styles.formContainer}>
+					<Text></Text>
+					<TextInput
+						label="Email"
+						value={text}
+						onChangeText={(text) => setText(text)}
+					/>
+				</View>
+			</View>
+			<View style={styles.nextBackBtnsContainer}>
+				<Button
+					style={styles.backButton}
+					color={CustomColors.uva.blue}
+					mode="contained"
+					onPress={() => {
+						decIndex();
+					}}
+				>
+					BACK
+				</Button>
+				<Button
+					style={styles.nextButton}
+					color={CustomColors.uva.blue}
+					mode="contained"
+					onPress={() => {
+						if (stepIndex + 1 <= chainSteps.length - 1) {
+							incrIndex();
+						} else {
+							setReadyToSubmit(true);
+							navigation.navigate("BaselineAssessmentScreen", {
+								session,
+							});
+						}
+					}}
+				>
+					NEXT
+				</Button>
+			</View>
+			{readyToSubmit && (
 				<Button
 					mode="contained"
 					onPress={() => {
 						navigation.navigate("ChainsHomeScreen");
 					}}
 				>
-					To the Chain
+					Submit
 				</Button>
-			</View>
+			)}
 		</ImageBackground>
 	);
 };
@@ -48,6 +101,22 @@ const BaselineAssessmentScreen: FC<Props> = (props) => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+	},
+	image: {
+		flex: 1,
+		resizeMode: "cover",
+	},
+	nextBackBtnsContainer: {
+		flexDirection: "row",
+		justifyContent: "flex-end",
+	},
+	nextButton: {
+		width: 144,
+		margin: 15,
+	},
+	backButton: {
+		width: 144,
+		margin: 15,
 	},
 });
 
