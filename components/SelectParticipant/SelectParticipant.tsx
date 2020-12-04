492d23f0376d7438a113e32bd7d1be47e27ae51d
Aaron Louie <ajlouie@gmail.com>
2020-12-04 17:26:50 -0500

    Checks for identification first.

--- components/SelectParticipant/SelectParticipant.tsx
+++ components/SelectParticipant/SelectParticipant.tsx
@@ -35,10 +35,12 @@ export const SelectParticipant = (props: SelectParticipantProps): ReactElement =
     await closeMenu();
   };
 
-  const participantName = (p: Participant): string => {
-    const first = p.identification.nickname || p.identification.first_name;
-    const last = p.identification.last_name;
-    return `${first} ${last}`;
+  const participantName = (p: Participant): string | undefined => {
+    if (p.identification) {
+      const first = p.identification.nickname || p.identification.first_name;
+      const last = p.identification.last_name;
+      return `${first} ${last}`;
+    }
   }
 
   useEffect(() => {
