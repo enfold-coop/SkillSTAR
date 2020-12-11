6cd431997c066c9b2b6394a36f56f73ca9d10f7f
Erik <kimseyerik@gmail.com>
2020-12-11 18:55:58 -0500

    Added prompt indicator icons as acronyms, png and svg.  tried prompt level indicators are mere text w/ border

--- components/DataVerification/DataVerifItem.tsx
+++ components/DataVerification/DataVerifItem.tsx
@@ -18,7 +18,19 @@ const MasteryIcons = (level: string) => {
 		focus: require("../../assets/icons/in-process-icon.png"),
 		notStarted: require("../../assets/icons/waving-icon.png"),
 	};
+	console.log(icons[`${level}`]);
 	return icons[`${level}`];
+};
+const PromptIcons = (level: string) => {
+	const icons = {
+		ip: require("../../assets/icons/ip_prompt_icon.png"),
+		sp: require("../../assets/icons/sp_prompt_icon.png"),
+		pp: require("../../assets/icons/pp_prompt_icon.png"),
+		fp: require("../../assets/icons/fp_prompt_icon.png"),
+	};
+
+	// return icons[`${level}`];
+	// return
 };
 
 type Props = {
@@ -29,10 +41,14 @@ const DataVerifItem: FC<Props> = ({ stepAttempt }) => {
 	/**
 	 * use context api, here:
 	 */
-	const { stepId, instruction } = stepAttempt;
+
+	const { stepId, instruction, promptLevel } = stepAttempt;
+	console.log(promptLevel);
+
 	const [promptSwitch, setPromptSwitch] = useState(false);
 	const [behavSwitch, setBehavSwitch] = useState(false);
 	const [icon, setIcon] = useState();
+	const [promptIcon, setPromptIcon] = useState();
 
 	const handlePromptSwitch = (v: boolean) => {
 		setPromptSwitch(!promptSwitch);
@@ -50,10 +66,24 @@ const DataVerifItem: FC<Props> = ({ stepAttempt }) => {
 			setIcon(MasteryIcons("notStarted"));
 		}
 	};
+	const promptLevelIcon = () => {
+		if (promptLevel === 4) {
+			setPromptIcon(PromptIcons("sp"));
+		} else if (promptLevel === 3) {
+			setPromptIcon(PromptIcons("sp"));
+		} else if (promptLevel === 2) {
+			setPromptIcon(PromptIcons("pp"));
+		} else {
+			setPromptIcon(PromptIcons("fp"));
+		}
+	};
 
 	useEffect(() => {
 		focusStepIcon();
 	}, [stepId]);
+	useEffect(() => {
+		promptLevelIcon();
+	}, [promptLevel]);
 
 	return (
 		<View style={styles.container}>
@@ -65,20 +95,27 @@ const DataVerifItem: FC<Props> = ({ stepAttempt }) => {
 				/>
 
 				<Text style={styles.stepTitle}>"{instruction}"</Text>
-				<Text style={styles.promptLevel}>{"[...]"}</Text>
-				<View style={styles.questionContainer}>
-					<PromptDataVerifSwitch
-						instruction={instruction}
-						id={stepId}
-						handleSwitch={handlePromptSwitch}
-					/>
-				</View>
-				<View style={styles.questionContainer}>
-					<BehavDataVerifSwitch
-						instruction={instruction}
-						id={stepId}
-						handleSwitch={handleBehavSwitch}
-					/>
+				<Text style={styles.promptLevelIcon}>{"PP"}</Text>
+				{/* <Image
+					style={styles.promptLevelIcon}
+					source={promptIcon}
+					resizeMode="contain"
+				/> */}
+				<View style={styles.switchContainer}>
+					<View style={styles.questionContainer}>
+						<PromptDataVerifSwitch
+							instruction={instruction}
+							id={stepId}
+							handleSwitch={handlePromptSwitch}
+						/>
+					</View>
+					<View style={styles.questionContainer}>
+						<BehavDataVerifSwitch
+							instruction={instruction}
+							id={stepId}
+							handleSwitch={handleBehavSwitch}
+						/>
+					</View>
 				</View>
 			</View>
 			<View style={styles.accordionContainer}>
@@ -110,7 +147,7 @@ const styles = StyleSheet.create({
 	},
 	defaultFormContainer: {
 		flexDirection: "row",
-		justifyContent: "space-evenly",
+		justifyContent: "space-between",
 		alignContent: "center",
 	},
 	accordionContainer: {},
@@ -121,14 +158,28 @@ const styles = StyleSheet.create({
 		borderRadius: 14,
 		borderWidth: 0,
 	},
-	promptLevel: {
-		width: "5%",
+	promptLevelIcon: {
+		// width: 30,
+		// height: 30,
+		fontSize: 20,
+		fontWeight: "800",
+		borderRadius: 5,
+		borderWidth: 2,
+		borderColor: CustomColors.uva.gray,
+		color: CustomColors.uva.gray,
+		padding: 5,
+		textAlign: "center",
 		alignSelf: "center",
 	},
 	stepTitle: {
-		width: "38%",
+		width: "30%",
 		alignSelf: "center",
 		fontSize: 20,
+	},
+	switchContainer: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignContent: "center",
 	},
 	questionContainer: {
 		flexDirection: "column",
