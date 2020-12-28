0938b71576209bf0d031fc2b7cc8dcb1357559ea
Erik <kimseyerik@gmail.com>
2020-12-28 13:50:19 -0500

    ...

--- components/Steps/StepAttemptStars.tsx
+++ components/Steps/StepAttemptStars.tsx
@@ -17,23 +17,25 @@ const StepAttemptStars: FC<Props> = (props) => {
 				<Text style={styles.promptTypeText}>
 					{props.promptType + ":"}
 				</Text>
-				{attemptsWPromptType.map((e) => {
+				{attemptsWPromptType.map((e, i) => {
 					if (e) {
 						return (
 							<AntDesign
 								name="star"
-								size={50}
+								size={40}
 								color={CustomColors.uva.orange}
 								style={styles.star}
+								key={i}
 							/>
 						);
 					} else {
 						return (
 							<AntDesign
 								name="staro"
-								size={50}
+								size={40}
 								color={CustomColors.uva.orange}
 								style={styles.star}
+								key={i}
 							/>
 						);
 					}
