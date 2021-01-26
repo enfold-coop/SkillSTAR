b3b4b0796484b1e19a004b1e504a1d3306744dbd
Erik <kimseyerik@gmail.com>
2021-01-26 07:49:18 -0500

    Fixed string lit error

--- screens/StepScreen.tsx
+++ screens/StepScreen.tsx
@@ -214,16 +214,21 @@ const StepScreen = (): JSX.Element => {
             <Text style={styles.needAddlPrompt}>{`Needed Add'l Prompting`}</Text>
             <Button
               style={styles.neededPromptingBtn}
-              labelStyle={{ fontSize: 24, paddingVertical: 5, color: CustomColors.uva.white }}
+              labelStyle={{ fontSize: 28, paddingVertical: 5, color: CustomColors.uva.white }}
               color={CustomColors.uva.orange}
               mode={'contained'}
               onPress={() => {
-                console.log('NEEDING PROMPTING');
+                if (stepIndex !== undefined && chainSteps && stepIndex + 1 <= chainSteps.length - 1) {
+                  incrIndex();
+                //   add 
+                } else {
+                  navigation.navigate('RewardsScreens');
+                }
               }}
             >{`+`}</Button>
             <Button
               style={styles.nextButton}
-              labelStyle={{ fontSize: 24, paddingVertical: 5 }}
+              labelStyle={{ fontSize: 24, paddingTop: 5, paddingBottom:0 }}
               color={CustomColors.uva.blue}
               mode={'contained'}
               onPress={() => {
