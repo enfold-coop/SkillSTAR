79b59f4abf66c08612cfb3ca30df5db9296c5c68
Aaron Louie <ajlouie@gmail.com>
2022-05-09 17:23:42 -0400

    Fixes breaking changes for EAS build process

--- data/videos.ts
+++ data/videos.ts
@@ -1,18 +1,33 @@
 import { AVPlaybackSource } from 'expo-av/build/AV';
 
+const toothbrushing01 = require('../assets/videos/toothbrushing_01.mp4');
+const toothbrushing02 = require('../assets/videos/toothbrushing_02.mp4');
+const toothbrushing03 = require('../assets/videos/toothbrushing_03.mp4');
+const toothbrushing04 = require('../assets/videos/toothbrushing_04.mp4');
+const toothbrushing05 = require('../assets/videos/toothbrushing_05.mp4');
+const toothbrushing06 = require('../assets/videos/toothbrushing_06.mp4');
+const toothbrushing07 = require('../assets/videos/toothbrushing_07.mp4');
+const toothbrushing08 = require('../assets/videos/toothbrushing_08.mp4');
+const toothbrushing09 = require('../assets/videos/toothbrushing_09.mp4');
+const toothbrushing10 = require('../assets/videos/toothbrushing_10.mp4');
+const toothbrushing11 = require('../assets/videos/toothbrushing_11.mp4');
+const toothbrushing12 = require('../assets/videos/toothbrushing_12.mp4');
+const toothbrushing13 = require('../assets/videos/toothbrushing_13.mp4');
+const toothbrushing14 = require('../assets/videos/toothbrushing_14.mp4');
+
 export const videos: { [key: string]: AVPlaybackSource } = {
-  step_1: require('../assets/videos/toothbrushing_01.mp4'),
-  step_2: require('../assets/videos/toothbrushing_02.mp4'),
-  step_3: require('../assets/videos/toothbrushing_03.mp4'),
-  step_4: require('../assets/videos/toothbrushing_04.mp4'),
-  step_5: require('../assets/videos/toothbrushing_05.mp4'),
-  step_6: require('../assets/videos/toothbrushing_06.mp4'),
-  step_7: require('../assets/videos/toothbrushing_07.mp4'),
-  step_8: require('../assets/videos/toothbrushing_08.mp4'),
-  step_9: require('../assets/videos/toothbrushing_09.mp4'),
-  step_10: require('../assets/videos/toothbrushing_10.mp4'),
-  step_11: require('../assets/videos/toothbrushing_11.mp4'),
-  step_12: require('../assets/videos/toothbrushing_12.mp4'),
-  step_13: require('../assets/videos/toothbrushing_13.mp4'),
-  step_14: require('../assets/videos/toothbrushing_14.mp4'),
+  step_1: toothbrushing01,
+  step_2: toothbrushing02,
+  step_3: toothbrushing03,
+  step_4: toothbrushing04,
+  step_5: toothbrushing05,
+  step_6: toothbrushing06,
+  step_7: toothbrushing07,
+  step_8: toothbrushing08,
+  step_9: toothbrushing09,
+  step_10: toothbrushing10,
+  step_11: toothbrushing11,
+  step_12: toothbrushing12,
+  step_13: toothbrushing13,
+  step_14: toothbrushing14,
 };
